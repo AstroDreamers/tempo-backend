92cd06e1d62c0eca56a38ce12608d4c716fff0f9
Andy <doanvietanh03022003@gmail.com>
2025-09-28 13:47:06 +1000

    initial commit

---
+++ src/main/java/com/andy/tempoapp/TempoappApplication.java
@@ -0,0 +1,13 @@
+package com.andy.tempoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempoappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempoappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andy/tempoapp/TempoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.tempoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempoappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
