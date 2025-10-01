33e67e4b469eeedc465a08e71fcf00faca75804f
Andy <doanvietanh03022003@gmail.com>
2025-10-01 15:56:01 +1000

    add ai conversation endpoint

---
+++ src/main/java/com/andy/tempoapp/dto/request/ChatRequestDto.java
@@ -0,0 +1,8 @@
+package com.andy.tempoapp.dto.request;
+
+import lombok.Data;
+
+@Data
+public class ChatRequestDto {
+    private String message;
+}
--- src/main/java/com/andy/tempoapp/service/client/OpenAIClient.java
+++ src/main/java/com/andy/tempoapp/service/client/OpenAIClient.java
@@ -1,10 +1,12 @@
 package com.andy.tempoapp.service.client;
 
-import com.andy.tempoapp.dto.request.AnalysisRequestDto;
+import com.andy.tempoapp.dto.request.AnalysisRequest;
 
 import java.util.concurrent.CompletableFuture;
 
 public interface OpenAIClient {
 
-    CompletableFuture<String> askWithData(AnalysisRequestDto analysisRequestDto);
+    CompletableFuture<String> askWithData(AnalysisRequest analysisRequestDto);
+
+    CompletableFuture<String> conversation(String chat);
 }
