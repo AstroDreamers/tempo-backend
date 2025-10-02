06d61c8d4ffa5e5c3bf1065bb2b75cfcf78fb9e7
Andy <doanvietanh03022003@gmail.com>
2025-10-03 00:31:23 +1000

    authen

--- src/main/java/com/andy/tempoapp/config/ApplicationConfiguration.java
+++ src/main/java/com/andy/tempoapp/config/ApplicationConfiguration.java
@@ -8,15 +8,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class ApplicationConfig {
+public class ApplicationConfiguration {
     private final UserRepository userRepository;
-    public ApplicationConfig(UserRepository userRepository) {
+    public ApplicationConfiguration(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
--- src/main/java/com/andy/tempoapp/entity/User.java
+++ src/main/java/com/andy/tempoapp/entity/User.java
@@ -2,7 +2,8 @@ package com.andy.tempoapp.entity;
 
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -14,33 +15,39 @@ import java.util.List;
 @Table(name = "users")
 @Getter
 @Setter
-@NoArgsConstructor @AllArgsConstructor
 public class User implements UserDetails {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     @Column(unique = true, nullable = false)
     private String username;
-
+    @Column(unique = true, nullable = false)
+    private String email;
     @Column(nullable = false)
     private String password;
 
+    @Column(name = "verification_code")
+    private String verificationCode;
+    @Column(name = "verification_expiration")
+    private LocalDateTime verificationCodeExpiresAt;
     private boolean enabled;
 
-    @Column(name = "verfircation_code")
-    private String verificationCode;
-
-
-    @Column(name = "verification_expiration")
-    private LocalDateTime verificationExpiration;
+    //constructor for creating an unverified user
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    //default constructor
+    public User(){
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
     }
 
+    //TODO: add proper boolean checks
     @Override
     public boolean isAccountNonExpired() {
         return true;
