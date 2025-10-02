30ea5f2afcb0746a3f4e18b0ada9ff2840a64b58
Andy <doanvietanh03022003@gmail.com>
2025-10-02 21:56:09 +1000

    test

---
+++ src/main/java/com/andy/tempoapp/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.andy.tempoapp.repository;
+
+import com.andy.tempoapp.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationCode(String verificationCode);
+}
